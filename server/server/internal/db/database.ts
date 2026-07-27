191f53f5b32dd5d3a51e1749dc65df0ce1595135
John Smith <you@example.com>
2026-07-27 08:07:00 -0400

    style: fix prettier formatting in database.ts

--- server/server/internal/db/database.ts
+++ server/server/internal/db/database.ts
@@ -15,4 +15,5 @@ const prisma = globalForPrisma.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prismaGlobal = prisma;
+if (process.env.NODE_ENV !== "production")
+  globalForPrisma.prismaGlobal = prisma;
